ab5637109324c8fcaa0d983afe419985ba6936ad
dayoung <kallzero1008@jbnu.ac.kr>
2024-11-17 14:19:27 +0900

    complete 10830

--- algorithm/baekjoon/10830.py
+++ algorithm/baekjoon/10830.py
@@ -8,50 +8,37 @@ def mulMat(M1, M2):
     for i in range(N):
         for j in range(N):
             for k in range(N):
-                M[i][j] += M1[i][k] * M2[k][j]
+                M[i][j] += M1[i][k] * M2[k][j] 
+                M[i][j] %= 1000
     
     return M
 
 def sqrS(B, M):
-    global N
-
     if B == 1:
-        return M
+        return [[val%1000 for val in row] for row in M]
     
     tmp_M = sqrS(B//2, M)
+    
     if B % 2 == 0:
         return mulMat(tmp_M, tmp_M)
     else:
         return mulMat(mulMat(tmp_M, tmp_M), M)
 
-    
-def divT(M):
-    global N
-
-    result = [[0]*N for _ in range(N)]
-
-    for i in range(N):
-        for j in range(N):
-            result[i][j] = M[i][j] % 1000
-
-    return result
-
 if __name__ == '__main__':
     # answer
     #     69 558
     #     337 406
-    inputs = [
-        '2 5\n',
-        '1 2\n',
-        '3 4\n'
-    ]
+    # inputs = [
+    #     '2 5\n',
+    #     '1 2\n',
+    #     '3 4\n'
+    # ]
 
-    # inputs = sys.stdin.readlines()
+    inputs = sys.stdin.readlines()
 
     N, B = map(int, inputs[0].split())
     matrix = [list(map(int, input.split())) for input in inputs[1:]]
-    sqr_matrix = sqrS(B, matrix)
-    result_matrix = divT(sqr_matrix)
+    result_matrix = sqrS(B, matrix)
     
     for row in result_matrix:
         print(*row)
