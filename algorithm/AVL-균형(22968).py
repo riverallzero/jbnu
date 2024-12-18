210d58a0213d3a1040822a8bd8c119a42d24c6a9
dayoung <kallzero1008@jbnu.ac.kr>
2024-12-19 00:34:04 +0900

    complete 22968

---
+++ algorithm/AVL-균형(22968).py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == '__main__':
+    inputs = sys.stdin.readlines()
+
+    for line in inputs[1:]:
+        v = int(line.strip())
+        a = 1
+        b = 2
+        max_height = 0
+        
+        while a <= v:
+            a, b = b, a + b + 1
+            max_height += 1
+        print(max_height)
