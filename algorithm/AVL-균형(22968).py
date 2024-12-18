0d433c54a2c54872e00b241d6367ae9ba63182d4
dayoung <kallzero1008@jbnu.ac.kr>
2024-12-19 00:36:24 +0900

    edit 22968

--- algorithm/AVL-균형(22968).py
+++ algorithm/AVL-균형(22968).py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         max_height = 0
         
         while a <= v:
-            a, b = b, a + b + 1
+            a, b = b, a + b + 1 # 피보나치 수열로 계산(수정) b = a + b -> b = a + b + 1
             max_height += 1
         print(max_height)
