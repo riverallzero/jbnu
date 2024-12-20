f64a2e01b69f4029f2d56ad165f73c9bb8afc589
Dayoung Kang <kallzero1008@jbnu.ac.kr>
2024-12-20 15:01:53 +0900

    Update BFS&DFS-돌리기(1682).py

--- algorithm/BFS&DFS-돌리기(1682).py
+++ algorithm/BFS&DFS-돌리기(1682).py
@@ -4,79 +4,103 @@
 
 from collections import deque
 
-def aTrans(input_list):
-    first_line = input_list[0], input_list[1], input_list[2], input_list[3]
-    second_line = input_list[7], input_list[6], input_list[5], input_list[4]
+answer = tuple(map(int, input().split()))
 
-    input_list[0], input_list[1], input_list[2], input_list[3] = second_line
-    input_list[7], input_list[6], input_list[5], input_list[4] = first_line
+visited = set()
+queue = deque([(0, (1, 2, 3, 4, 5, 6, 7, 8))])
+while queue:
+    i, numbers = queue.popleft()
 
-    return input_list
+    if numbers == answer:
+        print(i)
 
-def bTrans(input_list):
-    return [input_list[3]] + input_list[0:3] + input_list[5:7] + [input_list[7]] + [input_list[4]]
+        break
 
-def cTrans(input_list):
-    input_list[1], input_list[2], input_list[5], input_list[6] = input_list[2], input_list[5], input_list[6], input_list[1]
-
-    return input_list
-
-def dTrans(input_list):
-    input_list[0], input_list[4] = input_list[4], input_list[0]
-
-    return input_list
-
-def bfs(input_list, target_list):
-    if input_list == target_list:
-        return 0
-    
-    queue = deque([(input_list, 0)])
-    visited = set()
-
-    trans_alpha_list = ['a', 'b', 'c', 'd']
-    trans_typ_list = [aTrans, bTrans, cTrans, dTrans]
-    
-    while queue:
-        current_list, depth = queue.popleft()
-
-        if tuple(current_list) in visited:
-            continue
-        else:
-            visited.add(tuple(current_list))
-
-        counted = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
-        previous_alpha = None
-
-        for trans_alpha, trans_type in zip(trans_alpha_list, trans_typ_list):
-            if trans_alpha == previous_alpha:
-                counted[trans_alpha] += 1
-            else:
-                counted[trans_alpha] = 1
-
-            previous_alpha = trans_alpha
-
-            if (trans_alpha == 'a' or trans_alpha == 'd') and counted[trans_alpha] == 2:
-                counted[trans_alpha] = 0  
-                depth -= 2
-                continue  
+    for numbers in (
+            numbers[4:][::-1] + numbers[:4][::-1],
+            numbers[3:4] + numbers[0:3] + numbers[5:] + numbers[4:5],
+            numbers[:1] + numbers[2:3] + numbers[5:6] + numbers[3:4] + numbers[4:5] + numbers[6:7] + numbers[1:2] + numbers[7:],
+            numbers[4:5] + numbers[1:4] + numbers[:1] + numbers[5:]
+    ):
+        if numbers not in visited:
+            visited.add(numbers)
+            queue.append((i + 1, numbers))
             
-            if (trans_alpha == 'b' or trans_alpha == 'c') and counted[trans_alpha] == 4:
-                counted[trans_alpha] = 0  
-                depth -= 4
-                continue 
+# from collections import deque
 
-            trans_list = trans_type(current_list.copy())
+# def aTrans(input_list):
+#     first_line = input_list[0], input_list[1], input_list[2], input_list[3]
+#     second_line = input_list[7], input_list[6], input_list[5], input_list[4]
 
-            if trans_list == target_list:
-                return depth + 1
-            else:
-                queue.append((trans_list, depth + 1))
+#     input_list[0], input_list[1], input_list[2], input_list[3] = second_line
+#     input_list[7], input_list[6], input_list[5], input_list[4] = first_line
+
+#     return input_list
+
+# def bTrans(input_list):
+#     return [input_list[3]] + input_list[0:3] + input_list[5:7] + [input_list[7]] + [input_list[4]]
+
+# def cTrans(input_list):
+#     input_list[1], input_list[2], input_list[5], input_list[6] = input_list[2], input_list[5], input_list[6], input_list[1]
+
+#     return input_list
+
+# def dTrans(input_list):
+#     input_list[0], input_list[4] = input_list[4], input_list[0]
+
+#     return input_list
+
+# def bfs(input_list, target_list):
+#     if input_list == target_list:
+#         return 0
     
-if __name__ == '__main__':  
-    # answer = 5
-    # inputs = '6 4 2 8 1 3 5 7'
+#     queue = deque([(input_list, 0)])
+#     visited = set()
 
-    init_list = ['1', '2', '3', '4', '5', '6', '7', '8']
-    target_list = list(input().split(' '))
+#     trans_alpha_list = ['a', 'b', 'c', 'd']
+#     trans_typ_list = [aTrans, bTrans, cTrans, dTrans]
+    
+#     while queue:
+#         current_list, depth = queue.popleft()
 
-    print(bfs(init_list, target_list))
+#         if tuple(current_list) in visited:
+#             continue
+#         else:
+#             visited.add(tuple(current_list))
+
+#         counted = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+#         previous_alpha = None
+
+#         for trans_alpha, trans_type in zip(trans_alpha_list, trans_typ_list):
+#             if trans_alpha == previous_alpha:
+#                 counted[trans_alpha] += 1
+#             else:
+#                 counted[trans_alpha] = 1
+
+#             previous_alpha = trans_alpha
+
+#             if (trans_alpha == 'a' or trans_alpha == 'd') and counted[trans_alpha] == 2:
+#                 counted[trans_alpha] = 0  
+#                 depth -= 2
+#                 continue  
+            
+#             if (trans_alpha == 'b' or trans_alpha == 'c') and counted[trans_alpha] == 4:
+#                 counted[trans_alpha] = 0  
+#                 depth -= 4
+#                 continue 
+
+#             trans_list = trans_type(current_list.copy())
+
+#             if trans_list == target_list:
+#                 return depth + 1
+#             else:
+#                 queue.append((trans_list, depth + 1))
+    
+# if __name__ == '__main__':  
+#     # answer = 5
+#     # inputs = '6 4 2 8 1 3 5 7'
+
+#     init_list = ['1', '2', '3', '4', '5', '6', '7', '8']
+#     target_list = list(input().split(' '))
+
+#     print(bfs(init_list, target_list))
