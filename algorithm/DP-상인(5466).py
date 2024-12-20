994631aa45d47cfc16c70d711e808a30e4268520
dayoung <kallzero1008@jbnu.ac.kr>
2024-12-20 14:53:22 +0900

    complete 5466

--- algorithm/DP-상인(5466).py
+++ algorithm/DP-상인(5466).py
@@ -3,71 +3,67 @@
 # 여행을 끝마친 후 얻을 수 있는 최대 이익을 구하라
 
 import sys
+from collections import defaultdict
 
-def segmentTree(arr, l, r):
-    ans = INF
-
-    # 트리 인덱스 초기화
+def segTree(arr, l, r):
     l += MAXN
     r += MAXN
+    ans = -INF
 
     while l <= r:
         if l & 1: # l이 홀수인 경우 왼쪽 자식
-            ans = max(ans, arr[l]) # 최대값 갱신
-            l += 1 # l을 오른쪽 자식으로 이동
-        if not (r & 1): # r이 짝수인 경우 오른쪽 자식
-            ans = max(ans, arr[r]) # 최대값 갱신
-            r -= 1 # r을 왼쪽 자식으로 이동
-        l >>= 1 # l을 부모로 이동
-        r >>= 1 # r을 부모로 이동
+            ans = max(ans, arr[l]) 
+            l += 1 # 오른쪽 자식으로 이동
+        if not r & 1: # r이 짝수인 경우 오른쪽 자식
+            ans = max(ans, arr[r])
+            r -= 1 # 왼쪽 자식으로 이동
+        # 부모로 이동
+        l //= 2
+        r //= 2
 
     return ans
 
 def update(arr, x, v):
-    x += MAXN # 인덱스를 트리 중간으로 설정
-    arr[x] = max(arr[x], v) # 현재 위치에 최대값 저장
-
+    x += MAXN
+    arr[x] = max(arr[x], v)
     while x > 1:
-        arr[x >> 1] = max(arr[x], arr[x ^ 1]) # 부모 노드 갱신
-        x >>= 1 # x를 부모로 이동
+        x //= 2
+        arr[x] = max(arr[x * 2], arr[x * 2 + 1])
 
 def updateCost(x, v):
-    update(uptree, x, v - U * x) # 올라갈때 연료 비용 업데이트
-    update(downtree, x, v + D * x) # 내려갈때 연료 비용 업데이트
+    update(uptree, x, v - u * x)
+    update(downtree, x, v + d * x)
 
-def findMax(a): # uptree와 downtree를 비교해 최대값 도출
-    return max(segmentTree(downtree, 0, a) - D * a, segmentTree(uptree, a, MAXN - 1) + U * a)
+def findMax(a):
+    return max(segTree(downtree, 0, a) - d * a, segTree(uptree, a, MAXN - 1) + u * a)
 
 def processMarket(market):
     if not market:
-        return
-
-    market.sort() # Tk(시장이 열리는 날짜) 순으로 정렬
+        return 0
+    
+    market.sort()
     num_markets = len(market)
-    U_arr, D_arr = [0] * num_markets, [0] * num_markets
+    U = [-INF] * num_markets
+    D = [-INF] * num_markets
 
     # 시장 방문시 얻을 수 있는 최대 이익 계산
     for i in range(num_markets):
         temp = findMax(market[i][0])
-        U_arr[i] = temp
-        D_arr[i] = temp
+        U[i] = temp
+        D[i] = temp
 
-    # 내려갈 때 이익 계산
-    for i in range(1, num_markets):
-        D_arr[i] = max(D_arr[i], D_arr[i - 1] - D * (market[i][0] - market[i - 1][0]))
-
-    # 올라갈 때 이익 계산
-    for i in range(num_markets - 2, -1, -1):
-        U_arr[i] = max(U_arr[i], U_arr[i + 1] - U * (market[i + 1][0] - market[i][0]))
-
-    # 올라갈 때, 내려갈 때 이익 합산
     for i in range(num_markets):
-        D_arr[i] += market[i][1]
-        U_arr[i] += market[i][1]
+        if i > 0:
+            D[i] = max(D[i], D[i - 1] - d * (market[i][0] - market[i - 1][0]))
+        D[i] += market[i][1]
 
-    # 올라갈 때, 내려갈 때를 모두 고려한 최대 이익 계산
+    for i in range(num_markets - 1, -1, -1):
+        if i < num_markets - 1:
+            U[i] = max(U[i], U[i + 1] - u * (market[i + 1][0] - market[i][0]))
+        U[i] += market[i][1]
+
     for i in range(num_markets):
-        updateCost(market[i][0], max(U_arr[i], D_arr[i]))
+        updateCost(market[i][0], max(U[i], D[i]))
 
 if __name__ == '__main__':
     # answer = 50
@@ -81,21 +77,23 @@ if __name__ == '__main__':
 
     inputs = sys.stdin.readlines()
 
-    N, U, D, S = map(int, inputs[0].split())
+    n, u, d, s = map(int, inputs[0].split())
 
-    MAXN = 543210 # 트리 크기
-    INF = float('-inf') # 최솟값을 찾기 위한 음의 무한대
-    uptree = [INF] * (2 * MAXN) # 올라가는 경로에 대한 트리
-    downtree = [INF] * (2 * MAXN) # 내려가는 경로에 대한 트리
-    markets = [[] for _ in range(MAXN)] # 시장 위치 정보
+    MAXN = 500010 # 트리 크기
+    INF = float('inf') 
+    uptree = [-INF] * (2 * MAXN) # 올라가는 경로에 대한 트리
+    downtree = [-INF] * (2 * MAXN) # 내려가는 경로에 대한 트리
+    markets = defaultdict(list)
 
     for line in inputs[1:]:
         Tk, Lk, Mk = map(int, line.split())
         markets[Tk].append((Lk, Mk))
 
     # 집에서 출발할 때 0으로 초기화
-    updateCost(S, 0)
-    for i in range(1, 500002):
+    updateCost(s, 0)
+
+    for i in range(1, MAXN):
         processMarket(markets[i])
 
-    print(findMax(S))
+    print(findMax(s))
+    
