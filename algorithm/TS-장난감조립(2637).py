aa4168ba260d23780c62253e31c978171a11d364
dayoung <kallzero1008@jbnu.ac.kr>
2024-12-20 14:55:43 +0900

    complete 2637

--- algorithm/TS-장난감조립(2637).py
+++ algorithm/TS-장난감조립(2637).py
@@ -8,14 +8,19 @@ from collections import defaultdict, deque
 
 def solve(N, arr):
     graph = defaultdict(list)  # 그래프: X -> (Y, K)
-    degree_arr = [0] * (N + 1)  # 진입 차수
-    needs = [[0] * (N + 1) for _ in range(N + 1)]  # 각 부품별 필요 기본 부품 수
+    basic_part = [i for i in range(1, N + 1)] # 기본 부품
 
+    degree_arr = [0] * (N + 1)  # 진입 차수
     for x, y, k in arr:
         graph[y].append((x, k))
         degree_arr[x] += 1
 
-    # 진입 차수가 0인 노드부터 시작: 위상정렬
+        if x in basic_part:
+            basic_part.remove(x)
+
+    needs = [[0] * (N + 1) for _ in range(N + 1)]  # 각 부품별 필요 기본 부품 수
+
+    # 진입 차수가 0인 노드부터 시작
     que = deque()
     for i in range(1, N + 1):
         if degree_arr[i] == 0:
@@ -25,8 +30,8 @@ def solve(N, arr):
         curr = que.popleft()
 
         # 현재 부품이 기본 부품인 경우
-        for next_part, count in graph[curr]:
-            if all(needs[curr][j] == 0 for j in range(1, N + 1)):  # 기본 부품
+        for next_part, count in sorted(graph[curr]):
+            if curr in basic_part:  # 기본 부품
                 needs[next_part][curr] += count
             else:  # 중간 부품
                 for i in range(1, N + 1):
@@ -38,13 +43,10 @@ def solve(N, arr):
                 que.append(next_part)
 
     # 완제품(N)을 만드는 데 필요한 기본 부품만 출력
-    result = []
     for i in range(1, N):
         if needs[N][i] > 0:
-            result.append((i, needs[N][i]))
-
-    return result
-
+            print(f'{i} {needs[N][i]}')
+    
 if __name__ == '__main__':
     # answer = [
     #     1 16
@@ -70,10 +72,5 @@ if __name__ == '__main__':
     N = int(inputs[0])  
     M = int(inputs[1])  
     arr = [tuple(map(int, line.split())) for line in inputs[2:]]
-    arr = sorted(arr, key=lambda x: x[0], reverse=True)
 
-    results = solve(N, arr)
-
-    for idx, cnt in results:
-        print(idx, cnt)
-        
+    solve(N, arr)
